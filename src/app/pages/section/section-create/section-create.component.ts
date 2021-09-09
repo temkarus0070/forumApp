5ab80783e7e7c4253a0153f36b39f428025b566d
artyom <pretkovartyom@gmail.com>
2021-09-09 08:45:03 +0300

    add admin functions and sections

---
+++ src/app/pages/section/section-create/section-create.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {SectionService} from "../../../services/sectionService";
+import {Section} from "../../../models/Section";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-section-create',
+  templateUrl: './section-create.component.html',
+  styleUrls: ['./section-create.component.css']
+})
+export class SectionCreateComponent implements OnInit {
+  public nameControl:FormControl=new FormControl();
+  public formGroup:FormGroup=new FormGroup({"name":this.nameControl});
+
+  create(){
+    let section:Section={
+      name:this.nameControl.value,
+      id:0
+    };
+    this.sectionService.create(section).subscribe(
+      next=>{
+        this.router.navigateByUrl("/");
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+
+
+  constructor(private sectionService:SectionService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
